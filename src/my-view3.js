f0d814f26da90f3928369bbd10c4299b556afe7e
vipolian <polevit@yandex.ru>
2018-08-06 06:11:05 +0300

    Validator + Transport attempt

--- src/my-view3.js
+++ src/my-view3.js
@@ -1,11 +1,12 @@
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import './shared-styles.js';
-
+import Transport from './my-transport';
+import Validator from './my-validator';
 
 class MyView3 extends PolymerElement {
   static get template() {
-    return html`
+      return html`
       <style include="shared-styles">
         :host {
           display: block;
@@ -83,7 +84,7 @@ class MyView3 extends PolymerElement {
             <p>Password Confirm:</p>
             <input>
            
-           <div class="button">submit</div>
+           <div class="button" onclick="onSubmit()">submit</div>
           </div>     
            <div class="people"></div>
           </div>
@@ -91,13 +92,57 @@ class MyView3 extends PolymerElement {
     `;
   }
 
-    validate() {
-        let name = this.$.name.validate();
-        let email = this.$.email.validate();
-        let password = this.$.password.validate();
-        return name && email && password;
-    }
-
+    // inputs = [...this.getElementsByClassName('classToFind')];
+    // errorFields = [...this.getElementsByClassName('login-page_error')];
+    //
+    // showErrors (errors, errorFields, inputs) {
+    //     inputs.forEach((input, i) => {
+    //         input.addEventListener('blur', () => {
+    //             errorFields[i].innerHTML = '';
+    //             input.style.borderColor = 'dark-grey';
+    //             this.isValid([input], [errorFields[i]]);
+    //         });
+    //     });
+    //
+    //     errorFields.forEach((input, i) => {
+    //         errors.forEach((err) => {
+    //             if (input.getAttribute('name') === err.class[1]) {
+    //                 input.innerHTML = err.innerHTML;
+    //                 inputs[i].style.borderColor = '#E8175D';
+    //                 inputs[i].style.boxShadow = '0 0 15px 4px #E8175D';
+    //             }
+    //         });
+    //     });
+    // };
+    //
+    // isValid (inputs = [], errorFields = []) {
+    //     const fields = [...document.getElementsByClassName('classToFind')];
+    //     const errors = Validator(fields);
+    //     if (errors.length === 0) {
+    //         return true;
+    //     }
+    //     this.showErrors(errors, errorFields, inputs);
+    //     return false;
+    // };
+    //
+    // onSubmit() {
+    // if (this.isValid(this.inputs, this.errorFields)) {
+    //     let request = {};
+    //     this.inputs.forEach((input) => {
+    //         if (input.name === 'login') {
+    //             request.login = input.value;
+    //         }
+    //         if (input.name === 'email') {
+    //             request.email = input.value;
+    //         }
+    //         if (input.name === 'password') {
+    //             request.password = input.value;
+    //         }
+    //     });
+    //
+    //     Transport.Post(adr, request);
+    //     }
+    // }
 }
 
 window.customElements.define('my-view3', MyView3);
